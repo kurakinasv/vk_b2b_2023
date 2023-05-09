def496f5a30ba7bf120e9df517fd8ab2b410385f
kurakinasv <svetakurakina2002@gmail.com>
2023-05-10 00:22:51 +0300

    corrected project configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,9 +7,10 @@ module.exports = {
     'plugin:react-hooks/recommended',
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  settings: { react: { version: '17' } },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'no-unused-vars': 'warn',
   },
-}
+};
